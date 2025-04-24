94a76aa99f212763f2ca93de3c946c99404398cc
bbrockbrown <brockbrown46@gmail.com>
2025-04-24 17:30:12 -0500

    fix modules

--- .history/src/pages/Home_20250424155019.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from 'components/layouts/NavLayout';
-
-export default function Home() {
-
-  return(
-    <Navbar />
-  );
-}
--- .history/src/pages/Home_20250424161717.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from 'components/layouts/Navbar';
-
-export default function Home() {
-
-  return(
-    <Navbar />
-  );
-}
