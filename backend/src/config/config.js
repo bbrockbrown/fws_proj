535e8a195780e99482ffc434b3634b5cde6769ba
bbrockbrown <brockbrown46@gmail.com>
2025-05-04 23:30:33 -0500

    fixed keys

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,18 +1,14 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-if (!process.env.DB_INTERNAL_URL) {
+if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is required.");
 }
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false
   },
   max: 20,
   idleTimeoutMillis: 30000,
